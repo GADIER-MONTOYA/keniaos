b71dac00ef3b1c33d0f336ebbd4ce8df57c71c45
gadier <gadiermon@gmail.com>
2026-06-05 11:08:04 -0700

    disquecompleta

--- script.js
+++ script.js
@@ -4,69 +4,84 @@ const menuNavegacion = document.getElementById('menu-navegacion');
 const botonReproducir = document.getElementById('boton-reproducir');
 const iconoControl = document.getElementById('icono-control');
 const lineaTiempo = document.querySelector('.linea-tiempo');
+const tituloCancion = document.getElementById('titulo-cancion');
 
 const audio = document.getElementById('reproductor-audio');
 const botonAnterior = document.getElementById('boton-anterior');
 const botonSiguiente = document.getElementById('boton-siguiente');
 const imagenPortada = document.getElementById('imagen-portada');
 
-// Arreglo actualizado con tu lista real de canciones de 'kdekarma'
+//Lista de canciones
 const listaCanciones = [
     {
+        titulo: 'Belladona',
         archivo: 'kdekarma/Kenia Os - Belladona (SPOTISAVER).mp3',
         portada: 'imagenes/VinilRojo.png'
     },
     {
-        archivo: 'kdekarma/Kenia Os - Boom In Your Face (SPOTISAVER).mp3',
-        portada: 'imagenes/VinilAmarillo.png'
-    },
-    {
-        archivo: 'kdekarma/Kenia Os - Boys (SPOTISAVER).mp3',
-        portada: 'imagenes/VinilAzul.png'
-    },
-    {
-        archivo: 'kdekarma/Kenia Os - Breaking News (Interlude) (SPOTISAVER).mp3',
-        portada: 'imagenes/VinilRojo.png'
-    },
-    {
-        archivo: 'kdekarma/Kenia Os - Love Bombing (SPOTISAVER).mp3',
-        portada: 'imagenes/VinilAmarillo.png'
-    },
-    {
-        archivo: 'kdekarma/Kenia Os - Problemática (SPOTISAVER).mp3',
-        portada: 'imagenes/VinilAzul.png'
-    },
-    {
-        archivo: 'kdekarma/Kenia Os - Rojo Versace (SPOTISAVER).mp3',
-        portada: 'imagenes/VinilRojo.png'
-    },
-    {
-        archivo: 'kdekarma/Kenia Os - Ruleta Rusa (SPOTISAVER).mp3',
-        portada: 'imagenes/VinilAmarillo.png'
-    },
-    {
+        titulo: 'Slay',
         archivo: 'kdekarma/Kenia Os - Slay (SPOTISAVER).mp3',
-        portada: 'imagenes/VinilAzul.png'
-    },
-    {
-        archivo: 'kdekarma/Kenia Os - Tarde (SPOTISAVER).mp3',
         portada: 'imagenes/VinilRojo.png'
     },
     {
-        archivo: 'kdekarma/Kenia Os - Tú y Yo X Siempre (SPOTISAVER).mp3',
-        portada: 'imagenes/VinilAmarillo.png'
+        titulo: 'Días Tristes (con Carla Morrison)',
+        archivo: 'kdekarma/Kenia Os, Carla Morrison - Días Tristes (SPOTISAVER).mp3',
+        portada: 'imagenes/VinilAzul.png'
     },
     {
+        titulo: 'Una y Otra Vez',
         archivo: 'kdekarma/Kenia Os - Una y Otra Vez (SPOTISAVER).mp3',
         portada: 'imagenes/VinilAzul.png'
     },
     {
-        archivo: 'kdekarma/Kenia Os, Carla Morrison - Días Tristes (SPOTISAVER).mp3',
+        titulo: 'Tarde',
+        archivo: 'kdekarma/Kenia Os - Tarde (SPOTISAVER).mp3',
         portada: 'imagenes/VinilRojo.png'
     },
     {
+        titulo: 'Fifty Fifty (con Lola Indigo)',
         archivo: 'kdekarma/Kenia Os, Lola Indigo - Fifty Fifty (SPOTISAVER).mp3',
         portada: 'imagenes/VinilAmarillo.png'
+    },
+    {
+        titulo: 'Ruleta Rusa',
+        archivo: 'kdekarma/Kenia Os - Ruleta Rusa (SPOTISAVER).mp3',
+        portada: 'imagenes/VinilAmarillo.png'
+    },
+    {
+        titulo: 'Breaking News (Interlude)',
+        archivo: 'kdekarma/Kenia Os - Breaking News (Interlude) (SPOTISAVER).mp3',
+        portada: 'imagenes/VinilRojo.png'
+    },
+    {
+        titulo: 'Tú y Yo X Siempre',
+        archivo: 'kdekarma/Kenia Os - Tú y Yo X Siempre (SPOTISAVER).mp3',
+        portada: 'imagenes/VinilAzul.png'
+    },
+    {
+        titulo: 'Boys',
+        archivo: 'kdekarma/Kenia Os - Boys (SPOTISAVER).mp3',
+        portada: 'imagenes/VinilAmarillo.png'
+    },
+    {
+        titulo: 'Love Bombing',
+        archivo: 'kdekarma/Kenia Os - Love Bombing (SPOTISAVER).mp3',
+        portada: 'imagenes/VinilAzul.png'
+    },
+    {
+        titulo: 'Rojo Versace',
+        archivo: 'kdekarma/Kenia Os - Rojo Versace (SPOTISAVER).mp3',
+        portada: 'imagenes/VinilRojo.png'
+    },
+    {
+        titulo: 'Boom In Your Face',
+        archivo: 'kdekarma/Kenia Os - Boom In Your Face (SPOTISAVER).mp3',
+        portada: 'imagenes/VinilAmarillo.png'
+    },
+    {
+        titulo: 'Problematica',
+        archivo: 'kdekarma/Kenia Os - Problemática (SPOTISAVER).mp3',
+        portada: 'imagenes/VinilRojo.png'
     }
 ];
 
@@ -76,10 +91,10 @@ let indiceCancionActiva = 0;
 cargarCancion(listaCanciones[indiceCancionActiva]);
 lineaTiempo.classList.add('pausado');
 
-// Cargar pista en el reproductor HTML5
 function cargarCancion(cancion) {
     audio.src = cancion.archivo;
     imagenPortada.src = cancion.portada;
+    tituloCancion.innerText = cancion.titulo;
 }
 
 // Activar audio y animación
